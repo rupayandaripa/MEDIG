c0bf8cb29d82371506f7dd1aa168e2199b088a2b
iamrupayandaripa <rupayandaripa@gmail.com>
2024-12-11 00:31:27 +0530

    secrets removed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -31,7 +31,7 @@ const msalConfig = {
         clientId: process.env.CLIENT_ID,
         authority: `https://login.microsoftonline.com/${process.env.TENANT_ID}`,
         clientSecret: process.env.CLIENT_SECRET,
-        redirectUri: "http://localhost:8000/api/v0/users/auth/callback",
+        redirectUri: process.env.REDIRECT_URI,
     },
 };
 
